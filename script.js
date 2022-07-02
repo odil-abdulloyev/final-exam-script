62eb9bc3484ae73eb8d18817487e5ececfc6eb0c
Odil Abdulloyev <abdulloyevodil@mail.ru>
2022-07-02 19:57:52 +0500

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+const iframe = document.createElement('iframe');
+iframe.src = 'https://final-exam-six.vercel.app/';
+iframe.width = '100%';
+iframe.height = 300;
+iframe.style.border = 'none';
+document.body.append(iframe);
